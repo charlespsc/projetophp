4f53f2ddb42af5e00404c72fbfe9983122346fff
Charles Pereira <charlespereira.fln@gmail.com>
2023-08-14 14:27:43 -0300

    Operations file

---
+++ operacoes.php
@@ -0,0 +1,26 @@
+<?php
+
+//SOMA
+$idade = 46;
+$idadeDaqui10Anos = $idade + 10;
+#echo $idadeDaqui10Anos;
+
+//SUBTRAÇÃO
+$idadeDaqui10Anos = $idade - 10;
+#echo $idadeDaqui10Anos;
+
+//MULTIPLICAÇÃO
+$idadeDaqui10Anos = $idade * 10;
+#echo $idadeDaqui10Anos;
+
+//DIVISÃO
+$idadeDaqui10Anos = $idade / 10;
+#echo $idadeDaqui10Anos;
+
+//EXPONENCIAÇÃO
+$idadeDaqui10Anos = $idade ** 10;
+#echo $idadeDaqui10Anos;
+
+//RESTO DA DIVISÃO OU MÓDULO
+$idadeDaqui10Anos = $idade % 10;
+echo $idadeDaqui10Anos;
