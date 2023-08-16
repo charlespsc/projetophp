994362161968d2275a2953fb0a7653fe2b4868b1
Charles Pereira <charlespereira.fln@gmail.com>
2023-08-16 20:12:08 -0300

    Types file

---
+++ tipos.php
@@ -0,0 +1,22 @@
+<?php
+// TIPOS PRIMITIVOS
+
+$idade = 46; //integer
+$salario = 1000.30; //double ou float
+#echo gettype($salario);
+
+$divisao = 10 / 3;
+#echo gettype($divisao); // double
+$divisao = 3;
+#echo gettype($divisao); // integer
+
+$texto = "Olá Mundo"; // string
+#echo gettype($texto);
+
+$verdadeiro = true;
+$falso = false;
+#echo gettype($verdadeiro); //boolean
+echo gettype($falso);       //boolean
+
+
+
