fa1adc4fc5a8692cff45926426c28dffbd1695f7
Charles Pereira <charlespereira.fln@gmail.com>
2023-08-27 23:34:03 -0300

    String

--- strings.php
+++ strings.php
@@ -4,3 +4,8 @@ $idade = 46;
 echo 'Olá Mundo! Minha idade é ' . $idade . ' anos.';
 echo "Olá Mundo! Minha idade é $idade anos.";
 
+echo PHP_EOL . PHP_EOL;
+
+echo "Você só pode entrar, se tiver mais de 46 anos!\n";
+
+echo "Você tem $idade anos. Pode entrar!";
