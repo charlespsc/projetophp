ab2599511923226066c175728b6adec1b763367b
Charles Pereira <charlespereira.fln@gmail.com>
2023-08-27 23:23:20 -0300

    Escape e String

--- olaMundo.php
+++ olaMundo.php
@@ -2,4 +2,11 @@
 // comentario em uma linha
 /* comentario em
     varias linhas */
-echo "Olá Mundo!";
+echo "Olá Mundo!\n";
+$idade = 46;
+// echo "\n"    Quebra Linha
+// echo "\t"    Tab
+echo "Eu tenho $idade anos.";
+//Vamos quebrar linha com o PHP_EOL (End Of Line)
+echo "Olá Mundo!" . PHP_EOL;
+echo "Eu tenho $idade anos.";
---
+++ strings.php
@@ -0,0 +1,6 @@
+<?php
+
+$idade = 46;
+echo 'Olá Mundo! Minha idade é ' . $idade . ' anos.';
+echo "Olá Mundo! Minha idade é $idade anos.";
+
