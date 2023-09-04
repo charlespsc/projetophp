fb7e8251598de7a4b90403516743d18a81b4a1a1
Charles Pereira <charlespereira.fln@gmail.com>
2023-09-03 23:54:40 -0300

    Condicional if

--- decisoes.php
+++ decisoes.php
@@ -1,16 +1,21 @@
 <?php
 
-$idade = 17;
+$idade = 20;
+$numeroDePessoas = 1;
 
-echo "Você só pode entrar se tiver a partir de 18 anos." . PHP_EOL;
+echo "Você só pode entrar se tiver a partir de 18 anos ou a partir de 16 anos acompanhado." . PHP_EOL;
 
 // == é para dizer que é igual
 // || <=> or é para dizer OU
 // && <=> and  é para usar E
 
 if ($idade >= 18) {
-    echo "Você tem $idade anos." . PHP_EOL;
-    echo 'Pode entrar!';
+    echo "Você tem $idade anos. Pode entrar sozinho.";
+} else if ($idade >= 16 && $numeroDePessoas > 1) {
+        echo "Você tem $idade anos, está acompanhado(a), então pode entrar";
+} else {
+        echo "Você só tem $idade anos. Você não pode entrar!";
 }
 
-echo "Você só tem $idade anos. Você não pode entrar!";
+echo PHP_EOL;
+echo "Adeus!";
