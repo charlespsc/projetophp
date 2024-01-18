f8b1c69fb4c4dfd02c71323e4cf5c1258120d86c
Charles Pereira <charlespereira.fln@gmail.com>
2024-01-17 21:05:23 -0300

    update

--- decisoes.php
+++ decisoes.php
@@ -1,13 +1,13 @@
 <?php
 
-$idade = 20;
+$idade = 17;
 $numeroDePessoas = 1;
 
 echo "Você só pode entrar se tiver a partir de 18 anos ou a partir de 16 anos acompanhado." . PHP_EOL;
 
 // == é para dizer que é igual
-// || <=> or é para dizer OU
-// && <=> and  é para usar E
+// || or é para dizer OU
+// && and  é para usar E
 
 if ($idade >= 18) {
     echo "Você tem $idade anos. Pode entrar sozinho.";
