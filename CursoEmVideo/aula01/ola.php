f876199f36778894809c597fbbb3af2915053414
Charles Pereira <81668900+charlespsc@users.noreply.github.com>
2024-12-27 03:58:42 -0300

    Add files via upload

---
+++ CursoEmVideo/aula01/ola.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <title>Primeiro Exemplo PHP</title>
+    <style>
+        h2 {
+            color: red;
+            text-shadow: 1px 1px 1px black;
+        }
+    </style>
+</head>
+<body>
+<H1>Testando PHP</H1>
+    <?php
+        echo "<h2>Olá, mundo!</h2>";
+
+
+    ?>
+</body>
+
+</html>
+
